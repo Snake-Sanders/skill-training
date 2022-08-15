e9721a5fc54d26ee37266c8e498c9efc5bda927b
Snake Sanders <elmarian22@gmail.com>
2022-08-15 15:32:56 +0200

    added tailwind

--- phoenix/prag/config/dev.exs
+++ phoenix/prag/config/dev.exs
@@ -26,7 +26,9 @@ config :prag, PragWeb.Endpoint,
   secret_key_base: "Ay3wDJaFq9ltYTmE05fZyh5yMxRvmevGgsG58GV1RMCGlXknCGvJL9Uv/dJqvtfC",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    # Enable watcher in development
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
   ]
 
 # ## SSL Support
