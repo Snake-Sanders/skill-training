7778991d58fb5b35707e239bb5679382cfa2937b
Snake Sanders <elmarian22@gmail.com>
2022-08-16 08:05:49 +0200

    added light brightness range boundaries

--- phoenix/prag/lib/prag_web/live/light_live.ex
+++ phoenix/prag/lib/prag_web/live/light_live.ex
@@ -31,6 +31,10 @@ defmodule PragWeb.LightLive do
       <button phx-click="on">
         <img src="images/light-on.svg">
       </button>
+
+      <button phx-click="random">
+        <span>Light Me Up!</span>
+      </button>
     </div>
     """
   end
@@ -41,17 +45,22 @@ defmodule PragWeb.LightLive do
   end
 
   def handle_event("down", _, socket) do
-    socket = update(socket, :brightness, &(&1 - 10 ))
+    socket = update(socket, :brightness, &max((&1 - 10 ), 0))
     {:noreply, socket}
   end
 
   def handle_event("up", _, socket) do
-    socket = update(socket, :brightness, &(&1 + 10 ))
+    socket = update(socket, :brightness, &min((&1 + 10 ),100))
     {:noreply, socket}
   end
 
   def handle_event("off", _, socket) do
     socket = assign(socket, :brightness, 0)
+    {:noreply, socket}
+  end
+
+  def handle_event("random", _, socket) do
+    socket = assign(socket, :brightness, Enum.random(0..100))
     {:noreply, socket}
   end
 end
