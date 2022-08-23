dd80a3ec596e369d8a7f529e3a01495afed5f46d
Snake Sanders <elmarian22@gmail.com>
2022-08-23 20:32:19 +0200

    sorted vehicles

--- phoenix/prag/lib/prag_web/live/vehicles_live.ex
+++ phoenix/prag/lib/prag_web/live/vehicles_live.ex
@@ -12,13 +12,20 @@ defmodule PragWeb.VehiclesLive do
     page = String.to_integer(params["page"] || "1")
     per_page = String.to_integer(params["per_page"] || "5")
 
-    options = %{page: page, per_page: per_page}
+    paginate_options = %{page: page, per_page: per_page}
 
-    vehicles = Vehicles.list_vehicles(paginate: options)
+    sort_by = (params["sort_by"] || "id") |> String.to_atom()
+    sort_order = (params["sort_order"] || "asc") |> String.to_atom()
+
+    sort_options = %{sort_by: sort_by, sort_order: sort_order}
+
+    options = [paginate: paginate_options, sort: sort_options]
+
+    vehicles = Vehicles.list_vehicles(options)
 
     socket =
       assign(socket,
-        options: options,
+        options: Map.merge(paginate_options, sort_options),
         vehicles: vehicles
       )
 
@@ -40,23 +47,53 @@ defmodule PragWeb.VehiclesLive do
             socket,
             __MODULE__,
             page: socket.assigns.options.page,
-            per_page: per_page
+            per_page: per_page,
+            sort_order: socket.assigns.options.sort_order,
+            sort_by: socket.assigns.options.sort_by
           )
       )
 
     {:noreply, socket}
   end
 
-  defp pagination_link(socket, name, page, per_page, class) do
+  defp pagination_link(socket, name, page, options, class) do
     live_patch(name,
       to:
         Routes.live_path(
           socket,
           __MODULE__,
           page: page,
-          per_page: per_page
+          per_page: options.per_page,
+          sort_by: options.sort_by,
+          sort_order: options.sort_order
         ),
       class: class
     )
   end
+
+  defp sort_link(socket, text, sort_by, options) do
+    text =
+      case options do
+        %{sort_by: ^sort_by, sort_order: sort_order} -> text <> get_emoji(sort_order)
+        %{sort_by: _, sort_order: _} -> text
+      end
+
+    live_patch(text,
+      to:
+        Routes.live_path(
+          socket,
+          __MODULE__,
+          sort_by: sort_by,
+          sort_order: toggle_sort_order(options.sort_order),
+          page: options.page,
+          per_page: options.per_page
+        )
+    )
+  end
+
+  defp toggle_sort_order(:asc), do: :desc
+  defp toggle_sort_order(:desc), do: :asc
+
+  defp get_emoji(:asc), do: "👆"
+  defp get_emoji(:desc), do: "👇"
 end
