e9833fc837c0a2b2222cad1ad7baf3bcd8878141
Snake Sanders <elmarian22@gmail.com>
2022-08-28 16:39:20 +0200

    handling component msg

---
+++ phoenix/prag/lib/prag_web/live/components/sandbox_calculator_component.ex
@@ -0,0 +1,60 @@
+defmodule PragWeb.SandboxCalculatorComponent do
+  use PragWeb, :live_component
+
+  alias Prag.SandboxCalculator
+
+  def mount(socket) do
+    {:ok, assign(socket, length: nil, width: nil, depth: nil, weight: 0)}
+  end
+
+  def render(assigns) do
+    ~H"""
+    <form phx-change="calculate" phx-target={@myself}
+          phx-submit="get-quote">
+      <div class="field">
+        <label for="length">Length:</label>
+          <input type="number" name="length" value={@length} />
+        <span class="unit">feet</span>
+      </div>
+      <div class="field">
+        <label for="width">Width:</label>
+          <input type="number" name="width" value={@width} />
+        <span class="unit">feet</span>
+      </div>
+      <div class="field">
+        <label for="depth">Depth:</label>
+          <input type="number" name="depth" value={@depth} />
+        <span class="unit">inches</span>
+      </div>
+      <div class="weight">
+        You need <%= @weight %> pounds
+      </div>
+      <button type="submit">
+        Get Quote
+      </button>
+    </form>
+    """
+  end
+
+  def handle_event("calculate", params, socket) do
+    %{"length" => l, "width" => w, "depth" => d} = params
+
+    weight = SandboxCalculator.calculate_weight(l, w, d)
+
+    socket = assign(socket, length: l, width: w, depth: d, weight: weight)
+
+    {:noreply, socket}
+  end
+
+  def handle_event("get-quote", _params, socket) do
+    # `price` is not updaed in the assigns because it belongs to live_view
+    # not to this component.
+    weight = socket.assigns.weight
+    price = SandboxCalculator.calculate_price(weight)
+
+    # this message is send to this process but it is handle by the live_view
+    send(self(), {:totals, %{weight: weight, price: price}})
+
+    {:noreply, socket}
+  end
+end
--- phoenix/prag/lib/prag_web/live/sandbox_live.ex
+++ phoenix/prag/lib/prag_web/live/sandbox_live.ex
@@ -2,10 +2,10 @@ defmodule PragWeb.SandboxLive do
   use PragWeb, :live_view
 
   alias PragWeb.QuoteComponent
+  alias PragWeb.SandboxCalculatorComponent
 
   def mount(_params, _session, socket) do
-
-    {:ok, socket}
+    {:ok, assign(socket, weight: nil, price: nil)}
   end
 
   def render(assigns) do
@@ -13,13 +13,22 @@ defmodule PragWeb.SandboxLive do
     <h1>Build a sandbox</h1>
 
     <div id="sandbox">
-      <%= live_component QuoteComponent,
-          material: "sand",
-          weight: 10.0,
-          price: 15.0
+      <.live_component module={SandboxCalculatorComponent} id="comp1" />
 
-      %>
+      <%= if @weight do %>
+        <.live_component module={QuoteComponent} id="comp2"
+                          material="sand"
+                          weight={@weight}
+                          price={@price} />
+      <% end %>
+
     </div>
     """
+  end
+
+  def handle_info({:totals, %{weight: weight, price: price}}, socket) do
+    socket = assign(socket, weight: weight, price: price)
+    IO.puts("+++ LiveView assings:#{inspect(socket.assigns)}")
+    {:noreply, socket}
   end
 end
