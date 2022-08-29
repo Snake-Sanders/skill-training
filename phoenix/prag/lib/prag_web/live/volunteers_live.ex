6d7ea40ceeba458946b2aeaa3c941d4350b9d0d2
Snake Sanders <elmarian22@gmail.com>
2022-08-29 10:52:13 +0200

    added volunteers form

--- phoenix/prag/lib/prag_web/live/volunteers_live.ex
+++ phoenix/prag/lib/prag_web/live/volunteers_live.ex
@@ -19,42 +19,6 @@ defmodule PragWeb.VolunteersLive do
     {:ok, socket, temporary_assigns: [volunteers: []]}
   end
 
-  def handle_event("save", %{"volunteer" => params}, socket) do
-    # `create_volunteer` will try to insert a new item in the DB with the content of params
-    case Volunteers.create_volunteer(params) do
-      {:ok, volunteer} ->
-        # when the item was inserted in the database:
-        # updates the list of volunteers without having to render all the list again.
-        # when volunteer is created there is a new event handled by handle_info
-        # now prepares for the next volunteer
-        changeset = Volunteers.change_volunteer(%Volunteer{})
-        socket = assign(socket, changeset: changeset)
-        # to see the "Saving..." message in the button
-
-        {:noreply, socket}
-
-      {:error, %Ecto.Changeset{} = changeset} ->
-        # when the item was not inserted in the database:
-        # the current form is updated with the previous values so they are not lost.
-        socket = assign(socket, changeset: changeset)
-        {:noreply, socket}
-    end
-  end
-
-  def handle_event("validate", %{"volunteer" => attrs}, socket) do
-    # In order to display any validation fields, it is required to indicate the action
-    # of the changeset,
-    changeset =
-      %Volunteer{}
-      |> Volunteers.change_volunteer(attrs)
-      |> Map.put(:action, :insert)
-
-    # updating the changeset in the socket will trigger a render
-    socket = assign(socket, changeset: changeset)
-
-    {:noreply, socket}
-  end
-
   def handle_info({:volunteer_created, volunteer}, socket) do
     # this changes are reflected to the client with preprend event
     socket = update(socket, :volunteers, fn volunteers -> [volunteer | volunteers] end)
