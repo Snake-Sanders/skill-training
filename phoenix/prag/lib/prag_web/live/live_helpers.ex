2e51ad620568dcfda69c273dddcb64a9edf7a9ab
Snake Sanders <elmarian22@gmail.com>
2022-09-03 13:05:11 +0200

    handling current user

--- phoenix/prag/lib/prag_web/live/live_helpers.ex
+++ phoenix/prag/lib/prag_web/live/live_helpers.ex
@@ -3,10 +3,22 @@ defmodule PragWeb.LiveHelpers do
   alias Prag.Accounts
 
   def assign_current_user(socket, session) do
+    IO.puts("+++ Connected: #{connected?(socket)}")
     # assign_new will call get_use* only if the :current_user is not set
     # this reduce the DB querries from 3 to 2.
-    assign_new(socket, :current_user, fn ->
-      Accounts.get_user_by_session_token(session["user_token"])
-    end)
+
+    user =
+      case Map.has_key?(session, "user_token") do
+        false ->
+          nil
+
+        true ->
+          session["user_token"]
+          |> IO.inspect(lable: "++ THE TOKEN iS OK")
+          |> Accounts.get_user_by_session_token()
+      end
+
+    IO.puts("++ Assigning current user #{inspect user} ")
+    assign_new(socket, :current_user, fn -> user end)
   end
 end
