8361b15da41d7720c3634a9be741c9d7e53f304f
Snake Sanders <elmarian22@gmail.com>
2022-09-05 10:59:14 +0200

    added canvas refresh button

--- phoenix/prag/lib/prag_web/live/chart_live.ex
+++ phoenix/prag/lib/prag_web/live/chart_live.ex
@@ -7,7 +7,8 @@ defmodule PragWeb.ChartLive do
 
     {:ok,
      assign(socket,
-       chart_data: %{labels: labels, values: values}
+       chart_data: %{labels: labels, values: values},
+       current_reading: List.last(labels)
      )}
   end
 
@@ -17,10 +18,32 @@ defmodule PragWeb.ChartLive do
     <h1>Blood Sugar</h1>
     <canvas id="chart-canvas"
             phx-hook="LineChart"
-            data-chart-data={Jason.encode!(@chart_data)}></canvas>
+            data-chart-data={Jason.encode!(@chart_data)}>
+    </canvas>
+    <div class="text-center">
+    <button phx-click="get-reading">
+      Get Reading
+    </button>
+    </div>
+
     </div>
     """
   end
+
+  def handle_event("get-reading", _, socket) do
+    socket = update(socket, :current_reading, &(&1 + 1))
+
+    point = %{
+      label: socket.assigns.current_reading,
+      value: get_reading()
+    }
+
+    # sends to the client the new point
+    socket = push_event(socket, "new-point", point)
+
+    {:noreply, socket}
+  end
+
   defp get_reading() do
     Enum.random(70..180)
   end
