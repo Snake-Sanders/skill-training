7452f2004f9a7a6f43a46df2943f18baa662fdd4
Snake Sanders <elmarian22@gmail.com>
2022-09-09 14:51:34 +0200

    copy temps to static uploads

--- phoenix/prag/lib/prag_web/live/desks_live.ex
+++ phoenix/prag/lib/prag_web/live/desks_live.ex
@@ -29,6 +29,19 @@ defmodule PragWeb.DesksLive do
   end
 
   def handle_event("save", %{"desk" => params}, socket) do
+    # now the images are already uploaded
+    # 1. copy temp files
+    urls =
+      consume_uploaded_entries(socket, :photo, fn meta, entry ->
+        # processes each upload file, the files are cleared on func exit
+        dest = Path.join("priv/static/uploads", filename(entry))
+        File.cp!(meta.path, dest)
+        Routes.static_path(socket, "/uploads/#{filename(entry)}")
+      end)
+
+    # desk = %Desk{photo_url: urls}
+    params = Map.merge(params, %{"photo_url" => urls})
+
     case Desks.create_desk(params) do
       {:ok, _desk} ->
         changeset = Desks.change_desk(%Desk{})
@@ -48,12 +61,16 @@ defmodule PragWeb.DesksLive do
     {:noreply, assign(socket, changeset: changeset)}
   end
 
-  def handle_event("cancel", %{"ref" => ref}, socket ) do
-
+  def handle_event("cancel", %{"ref" => ref}, socket) do
     {:noreply, cancel_upload(socket, :photo, ref)}
   end
 
   def handle_info({:desk_created, desk}, socket) do
     {:noreply, update(socket, :desks, fn desks -> [desk | desks] end)}
+  end
+
+  defp filename(entry) do
+    [ext | _] = MIME.extensions(entry.client_type)
+    "#{entry.uuid}.#{ext}"
   end
 end
