912001c1712cb29fe02722789049859ed9e48cb3
Snake Sanders <elmarian22@gmail.com>
2022-10-01 12:47:01 +0200

    reducing db access for auth

--- phoenix/pento/lib/pento_web/live/user_auth_live.ex
+++ phoenix/pento/lib/pento_web/live/user_auth_live.ex
@@ -4,10 +4,16 @@ defmodule PentoWeb.UserAuthLive do
   alias Pento.Accounts
 
   def on_mount(_, _paramas, %{"user_token" => user_token}, socket) do
-    user = Accounts.get_user_by_session_token(user_token)
+    # When a LiveView is mounted in a disconnected state, the Plug.Conn
+    # assigns will be available for reference via assign_new/3, allowing
+    # assigns to be shared for the initial HTTP request.
 
-    socket = assign(socket, :current_user, user)
+    socket =
+      assign_new(socket, :current_user,
+          fn -> Accounts.get_user_by_session_token(user_token) end
+        )
 
+    # if the user was found in the DB or it was already logged in, then continue
     if socket.assigns.current_user do
       {:cont, socket}
     else
